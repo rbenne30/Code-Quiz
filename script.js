6ed89d5fbb99ecffe11754eaa732d2ee8ec4b51e
rbenne30 <ryannebennett1@outlook.com>
2022-03-04 15:51:20 -0500

    adding an array to script.js

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
+const questions = [
+    "which Variable(s) are considered global", 
+    "Inside Which HTML element do we put the Javascript", 
+    "Where is the correct place to insert a Javascript", 
+    "The external Javascript file must contain the <script> tag.",
+];
+const questionsTwo = [
+    {
+      questionTitle:   "which Variable(s) are considered global", 
+      choiceA: "input",
+      choiceB: "input and sum",
+      choiceC: "sum",
+      choiceD: "sum num",
+    
+    },
+    {
+        questionTitleTwo: "inside Which HTML element do we put the Javascript", 
+        choiceA: "<js>",
+        choiceB: "<script>",
+        choiceC: "<scripting>",
+        choiceD: "<javascript>",
+    },
+]
+
 console.log("connected")
-var startBtn =document.querySelector("#start-btn")
-console.log (startBtn)
-function startQuiz(){
-    console.log("click")
-    var questionsection= document.getElementById("questions");
-    questionsection.classList.remove("hide")
+var startBtn = document.querySelector("#start-btn")
+console.log(startBtn)
+function showQuestions (){
+    var questionTitle = document.querySelector("#question-title")
+    console.log(questionTitle)
+    questionTitle.textContent = questionsTwo[0].questionTitle
+    var buttonA = document.querySelector("#choiceA")
+    console.log(buttonA)
+    buttonA.textContent = questionsTwo[0].choiceA
+    console.log(questions[0])
+    var buttonB = document.querySelector("#choiceB")
+    buttonB.textContent = questionsTwo[0].choiceB
+    var buttonC = document.querySelector("#choiceC")
+    buttonC.textContent = questionsTwo[0].choiceC
+    var buttonD = document.querySelector("#choiceD")
+    buttonD.textContent = questionsTwo[0].choiceD
+    
+    var questionTitleTwo = document.querySelector("#question-titletwo")
+    console.log(questionTitleTwo)
+    questionTitleTwo.textContent = questionsTwo[1].questionTitleTwo
+    var buttonE = document.querySelector("choiceE")
+    buttonE.textContent = questionsTwo[1].choiceE
+    var buttonF = document.querySelector("choiceF")
+    buttonF.textContent = questionsTwo[1].choiceF
+    var buttonG = document.querySelector("choiceG")
+    buttonG.textContent = questionsTwo[1].choiceG
+    var buttonH = document.querySelector("choiceH")
+    buttonH.textContent = questionsTwo[1].choiceH
+
 }
-startBtn.addEventListener("click" , startQuiz)
+function startQuiz() {
+    console.log("click")
+    var questionsection = document.getElementById("questions");
+    questionsection.classList.remove("hide")
+    showQuestions()
+}
+
+startBtn.addEventListener("click", startQuiz)
+
