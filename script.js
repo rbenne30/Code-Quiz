3ae2f3e3e5fff5253ebec48c860e66557bbff905
rbenne30 <ryannebennett1@outlook.com>
2022-03-05 13:51:09 -0500

    updated script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const quizEl = document.querySelector('.quiz');
+
 
 let clock = 60;
 let qIndex = 0;
@@ -12,20 +12,8 @@ const questions = [
 
 const ans = ['sum','js','the body section','true']
 
-console.log("connected")
 var startBtn = document.querySelector("#start-btn")
-console.log(startBtn)
-
-   
-function startQuiz() {
-    console.log("click")
-    var questionsection = document.getElementById("questions");
-    questionsection.classList.remove("hide")
-    showQuestions()
-}
-
-
-startBtn.addEventListener("click", startQuiz)
+startBtn.addEventListener("click", startBtn)
 
 document.querySelector('.answers').addEventListener('click', (e)=> console.log(e.target.innerText));
 
@@ -35,7 +23,7 @@ function handleClock() {
     document
         .querySelector('#time')
         .innerText = clock;
-    // document.querySelector('#q'+qIndex).classList.remove('hide')
+     //document.querySelector('#q'+qIndex).classList.remove('hide')
     qIndex++;
     clock--;
     if(clock<1) {
